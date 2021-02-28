f954a45dba75c2e48a35ce8c73fe9e4b2867085a
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-28 14:37:50 -0500

    adding schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS emp_traxDB;
+
+CREATE DATABASE emp_traxDB;
+
+USE emp_traxDB;
+
+CREATE TABLE department (
+    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(50),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES role(id)
+);
