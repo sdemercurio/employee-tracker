6e06a36f50e0a1d1a3ffed161811357a9756b30d
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-03-01 17:19:42 -0500

    starting over

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,21 @@ CREATE DATABASE emp_traxDB;
 USE emp_traxDB;
 
 CREATE TABLE department (
-    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    id INT NOT NULL auto_increment PRIMARY KEY,
     name VARCHAR(30),
 );
 
 CREATE TABLE role (
-    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    id INT NOT NULL auto_increment PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL auto_incremenet PRIMARY KEY,
+    id INT NOT NULL auto_increment PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(50),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,11 @@
 INSERT INTO department (name)
 VALUES
-('Management'),
-('Human Resources'),
-('Purchasing'),
-('Sales'),
-('Customer Service'),
-('Warehouse');
+
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('General Manager', 120000, 1),
-('Head of Human Resources', 80000, 2),
-('Head of Purchasing', 75000, 3),
-('Sales Rep', 60000, 4),
-('Customer Service Rep', 40000, 5),
-('Warhouse Employee', 40000, 6);
+
 
 INSERT INTO employee(first_name, last_name, role_id)
 VALUES
-('Mia', 'Wallace', 1, 1),
-('Vincent', 'Vega', 2, 2),
-('Winston', 'Wolf', 3, 3),
-('Jules', 'Winnfield', 4, 4),
-('Butch', 'Coolidge', 5, 5),
-('Captain', 'Koons', 6, 6);
 
