7b311fc88863e1ac5a6e1d6ebe9652469622c14e
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-03-02 15:27:43 -0500

    we have a connection and intial prompts working

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // viewing all employees by manager
 
-//Get sql statemtnts set first
+// Get sql statemtnts set first
 
-// Nested inquirere stuff
+// Nested inquirer stuff
 
 //department:
 // id - INT PRIMARY KEY
@@ -22,23 +22,16 @@
 // manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
 // Build a command-line application that at a minimum allows the user to:
 
-// Add departments, roles, employees
-
-// View departments, roles, employees
-
-// Update employee roles
-
+// =============== requiring modules =====================
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const contab = require('console.table');
-const express = require('express');
+const cTable = require('console.table');
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+// =============== establishing connection =====================
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
+    port: 3306,
     user: 'root',
     password: 'sarah123',
     database: 'emp_traxDB',
@@ -52,6 +45,40 @@ connection.connect((err) => {
     console.log(`connected as id ${connection.threadId}`);
 });
 
-// employee self referejntial
+// =============== inquirer prompts =====================
 
-//join of tables
+// What would you like to do?
+
+const init = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'View All employees',
+            'View All Employees By Department',
+            'View All Employees By Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+        ],
+    })
+};
+
+
+init();
+
+
+// =============== functions =====================
+
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
+
+// =============== bonus =====================
+
+// Update employee managers
+// View employees by manager
+// Delete departments, roles, and employees
+// View the total utilized budget of a department -- ie the combined salaries of all employees in that department
